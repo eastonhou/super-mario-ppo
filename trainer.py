12c89c25ab1983093f47d5d73cfd4133b97a24df
easton <easton.hou@gmail.com>
2023-06-06 19:07:11 +0800

    .

--- trainer.py
+++ trainer.py
@@ -27,7 +27,7 @@ class PPO:
         env = environment.MultiTrainEnv(self.game_creator, self.game_arguments, functools.partial(self._sample, device=device), self.skip)
         while True:
             self.logger.new_epoch()
-            self._train_epoch(env, 100, device)
+            self._train_epoch(env, 10, 8, device)
             self._eval_epoch(device)
             self.logger.end_epoch(self.epoch)
             if self.logger.rewards > self.model.rewards:
@@ -37,28 +37,41 @@ class PPO:
             else:
                 print(f'MODEL CONTINUE [reward={self.logger.rewards:>.2F}/{self.model.rewards:>.2F}]')
 
-    def _train_epoch(self, env, rounds, device):
+    def _train_epoch(self, env, rounds, batch_size, device):
         self.model.train()
         tq = tqdm.tqdm(range(rounds))
         for _ in tq:
             #ref_model = self._create_reference_model(device)
-            samples = env.get()
-            last_state = samples[-1]['next_state']
-            states = torch.tensor(np.stack([x['state'] for x in samples] + [last_state], 0), device=device)
-            actions = [x['action'] for x in samples]
-            rewards = torch.tensor([x['reward'] for x in samples], device=device)
-            with torch.no_grad():
-                ref_logits, ref_values = self.model(states)
-                R, advantages, ref_log_probs = self.criterion.precomute(ref_logits, ref_values, actions, rewards)
-            loss = self._offline(states[:-1], R, advantages, ref_log_probs, actions, rewards)
+            batch = [self._precompute_step(env, device) for _ in range(batch_size)]
+            loss = self._offline(batch)
             tq.desc = f'[{loss:>.2F}]'
             self.logger.train_step(loss)
 
-    def _offline(self, states, R, advantages, ref_log_probs, actions, rewards):
+    def _precompute_step(self, env, device):
+        samples = env.get()
+        last_state = samples[-1]['next_state']
+        states = torch.tensor(np.stack([x['state'] for x in samples] + [last_state], 0), device=device)
+        actions = [x['action'] for x in samples]
+        rewards = torch.tensor([x['reward'] for x in samples], device=device)
+        with torch.no_grad():
+            ref_logits, ref_values = self.model(states)
+            R, advantages, ref_log_probs = self.criterion.precomute(ref_logits, ref_values, actions, rewards)
+            return {
+                'states': states[:-1],
+                'actions': actions,
+                'rewards': rewards,
+                'R': R,
+                'advantages': advantages,
+                'ref_log_probs': ref_log_probs
+            }
+
+    def _offline(self, batch):
         total_loss = 0
         for _ in range(10):
-            logits, values = self.model(states)
-            loss = self.criterion(R, advantages, ref_log_probs, logits, values, actions, rewards)
+            loss = 0
+            for pack in batch:
+                logits, values = self.model(pack['states'])
+                loss += self.criterion(pack, logits, values)
             self.optimizer.zero_grad()
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
@@ -121,15 +134,15 @@ class Loss(nn.Module):
         coef = torch.pow(gamma * lmbda, torch.cumsum(torch.triu(torch.ones(512, 512), 1), -1))
         self.register_buffer('coef', torch.triu(coef, 0))
 
-    def forward(self, R, advantages, ref_log_probs, logits, values, actions, rewards):
-        n = ref_log_probs.shape[0]
+    def forward(self, pack, logits, values):
+        n = pack['ref_log_probs'].shape[0]
         new_log_probs = logits.log_softmax(-1)
-        ratio = (new_log_probs[torch.arange(n), actions] - ref_log_probs).exp()
+        ratio = (new_log_probs[torch.arange(n), pack['actions']] - pack['ref_log_probs']).exp()
         actor_loss = -torch.min(
-            ratio.mul(advantages),
-            torch.clamp(ratio, 1 - self.epsilon, 1 + self.epsilon).mul(advantages)).mean()
+            ratio.mul(pack['advantages']),
+            torch.clamp(ratio, 1 - self.epsilon, 1 + self.epsilon).mul(pack['advantages'])).mean()
         #critic_loss = (R.sub(values[:n]) ** 2).mean()
-        critic_loss = nn.functional.smooth_l1_loss(R, values, reduction='mean')
+        critic_loss = nn.functional.smooth_l1_loss(pack['R'], values, reduction='mean')
         entropy_loss = -new_log_probs.exp().mul(new_log_probs).sum(-1).mean()
         loss = actor_loss + critic_loss - self.beta * entropy_loss
         return loss
